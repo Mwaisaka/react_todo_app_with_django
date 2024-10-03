5c40615ce4dce67ee450756e0fdcaafdc530b555
Frank <frankkashere@gmail.com>
2024-10-03 20:39:10 +0300

    Initial commit

--- todo_app/client/src/components/Home/Home.jsx
+++ todo_app/client/src/components/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+import ToDoWrapper from "../ToDo/ToDoWrapper";
 
 function Home() {
   return (
     <div >
       <h3 className="text-2xl mt-10">Welcome to my To Do App</h3>
-      
+      <ToDoWrapper />
     </div>
   );
 }
