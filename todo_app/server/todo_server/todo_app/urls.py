1e9a7397c5c6bed2a8fcc5080305794756c1357a
Frank <frankkashere@gmail.com>
2024-10-14 19:53:27 +0300

    Updated Models

--- todo_app/server/todo_server/todo_app/urls.py
+++ todo_app/server/todo_server/todo_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name='todo_app'),
-    path('todo_app/',views.todo, name='todo_app'),
+    # path('',views.home, name='todo_app'),
+    path('',views.todo, name='todo'),
+    path('tasks/',views.tasks, name='todo'),
 ]
--- todo_app/server/todo_server/todo_app/views.py
+++ todo_app/server/todo_server/todo_app/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
-
+from .models import Task
 from django.http import HttpResponse
 from django.template import loader
+
 
 def todo(request):
     # return HttpResponse("Hello World!Welcome to the ToDo App Home Page!")
     template = loader.get_template('todoapp.html')
     return HttpResponse(template.render())
 
-def home(request):
-    return HttpResponse("Hello World!Welcome to the ToDo App Home Page!")
+def tasks(request):
+  mytasks = Task.objects.all().values()
+  template = loader.get_template('tasks.html')
+  context = {
+    'mytasks': mytasks,
+  }
+  return HttpResponse(template.render(context, request))
+# def home(request):
+#     return HttpResponse("Hello World!Welcome to the ToDo App Home Page!")
     
 # Create your views here.
