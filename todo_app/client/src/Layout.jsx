a99725e7ad8b70f8f252d7cb678cf770f57a3a96
Frank <frankkashere@gmail.com>
2024-10-24 20:22:48 +0300

    Updated Layout Component

--- todo_app/client/src/Layout.jsx
+++ todo_app/client/src/Layout.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.jsx';
 
 function Layout({ user, onLogout }) {
   return (
-    <div style={{width:"700px"}}>
+    <div className="flex flex-col min-h-screen w-full" 
+    // style={{minWidth:"500px",width:"auto"}}
+    >
+      <ScrollToTop />
       <Header user={user} onLogout={onLogout} />
-      <Outlet />
+      <main className="flex-grow w-full max-w-screen-3xl mx-auto px-8 sm:px-16 lg:px-4 py-2 lg:py-1">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
