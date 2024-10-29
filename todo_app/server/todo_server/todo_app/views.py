304d5c144e583106b9ef63306fcecdba6bca3e11
Frank <frankkashere@gmail.com>
2024-10-29 22:12:55 +0300

    Updated front end code

--- todo_app/server/todo_server/todo_app/views.py
+++ todo_app/server/todo_server/todo_app/views.py
@@ -9,39 +9,39 @@ from datetime import datetime
 
 # Create your views here.
 
-# def main(request):
-#     # return HttpResponse("Hello World!Welcome to the ToDo App Home Page!")
-#     template = loader.get_template('main.html')
-#     return HttpResponse(template.render())
+def main(request):
+    return HttpResponse("Hello World!Welcome to the ToDo App Home Page!")
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
 
-# def tasks(request):
-#   mytasks = Task.objects.all().values()
-#   template = loader.get_template('tasks.html')
-#   context = {
-#     'mytasks': mytasks,
-#   }
-#   return HttpResponse(template.render(context, request))
+def tasks(request):
+  mytasks = Task.objects.all().values()
+  template = loader.get_template('tasks.html')
+  context = {
+    'mytasks': mytasks,
+  }
+  return HttpResponse(template.render(context, request))
 
 def tasks(request):
     tasks = Task.objects.all().values()
     return JsonResponse(list(tasks), safe=False)
 
-# def details(request,id):
-#   mytask = Task.objects.get(id=id)
-#   template = loader.get_template('details.html')
-#   context = {
-#     'mytask': mytask,
-#   }
-#   return HttpResponse(template.render(context, request))
+def details(request,id):
+  mytask = Task.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mytask': mytask,
+  }
+  return HttpResponse(template.render(context, request))
 
-# def testing(request):
-#   template = loader.get_template("template.html")
-#   context = {
-#     'tasks': ['Reading', 'Hiking', 'Swimming'], 
-#   }
-#   return HttpResponse(template.render(context, request))
-# def home(request):
-#     return HttpResponse("Hello World!Welcome to the ToDo App Home Page!")
+def testing(request):
+  template = loader.get_template("template.html")
+  context = {
+    'tasks': ['Reading', 'Hiking', 'Swimming'], 
+  }
+  return HttpResponse(template.render(context, request))
+def home(request):
+    return HttpResponse("Hello World!Welcome to the ToDo App Home Page!")
     
 @csrf_exempt  # Exempting CSRF for API requests (can be handled better for production)
 def add_task(request):
