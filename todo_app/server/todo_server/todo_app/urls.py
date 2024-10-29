a79bf6d8ae7632c21c89bede5b8319446b44f6fa
Frank <frankkashere@gmail.com>
2024-10-29 22:03:41 +0300

    Updated front end code

--- todo_app/server/todo_server/todo_app/urls.py
+++ todo_app/server/todo_server/todo_app/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('',views.home, name='todo_app'),
-    # path('',views.main, name='main'),
+    path('',views.home, name='todo_app'),
+    path('',views.main, name='main'),
     path('tasks/',views.tasks, name='tasks'),
-    # path('tasks/details/<int:id>',views.details, name='details'),
-    #   path('testing/',views.testing, name='testing'),
+    path('tasks/details/<int:id>',views.details, name='details'),
+    path('testing/',views.testing, name='testing'),
     path('tasks/add/', views.add_task, name='add_task'),
     path('tasks/delete/<int:id>/', views.delete_task, name='delete_task'),  
     path('tasks/edit/<int:id>/', views.edit_task, name='edit_task'),
